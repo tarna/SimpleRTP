5dbe4ff2de6738e0867406dcb936630d293c04d9
tarna <46247728+tarna@users.noreply.github.com>
2025-01-04 16:37:19 -0500

    feat: updated to 1.21.4

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,50 +2,44 @@ import net.minecrell.pluginyml.bukkit.BukkitPluginDescription
 import xyz.jpenilla.runpaper.task.RunServer
 
 plugins {
-    kotlin("jvm") version "1.8.21"
-    kotlin("plugin.serialization") version "1.9.0"
+    kotlin("jvm") version "2.0.20"
 
-    id("com.github.johnrengelman.shadow") version "8.1.1"
+    id("com.gradleup.shadow") version "8.3.5"
 
-    id("net.minecrell.plugin-yml.bukkit") version "0.5.3"
-    id("xyz.jpenilla.run-paper") version "2.1.0"
+    id("net.minecrell.plugin-yml.bukkit") version "0.6.0"
+    id("xyz.jpenilla.run-paper") version "2.3.1"
 }
 
 group = "dev.tarna"
-version = "1.0.0"
-
-allprojects {
-    apply(plugin = "org.jetbrains.kotlin.jvm")
-
-    repositories {
-        mavenCentral()
-
-        maven("https://hub.spigotmc.org/nexus/content/repositories/snapshots/")
-    }
-
-    dependencies {
-        compileOnly("org.spigotmc:spigot-api:1.20.1-R0.1-SNAPSHOT")
-    }
-
-    kotlin {
-        jvmToolchain(17)
-    }
-}
+version = "1.1.0"
 
 repositories {
     maven("https://jitpack.io")
 }
 
+repositories {
+    mavenCentral()
+
+    maven("https://oss.sonatype.org/content/repositories/snapshots")
+    maven("https://hub.spigotmc.org/nexus/content/repositories/snapshots")
+}
+
 dependencies {
+    compileOnly("org.spigotmc:spigot-api:1.21.4-R0.1-SNAPSHOT")
+}
+
+kotlin {
+    jvmToolchain(21)
 }
 
 tasks.withType<RunServer> {
-    minecraftVersion("1.20.1")
+    minecraftVersion("1.21.4")
+    jvmArgs("-DPaper.IgnoreJavaVersion=true", "-Dcom.mojang.eula.agree=true")
 }
 
 bukkit {
     name = "SimpleRTP"
-    apiVersion = "1.20"
+    apiVersion = "1.21"
     version = "${project.version}"
     authors = listOf("Tarna")
     main = "dev.tarna.rtp.RTP"
